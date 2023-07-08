417bc3a93d5d6b94bcee1520e5eded63b64a7cc4
DuanerEdu <139003362+DuanerEdu@users.noreply.github.com>
2023-07-08 13:36:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,23 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
-
+from google.colab import drive
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 # Loading dataset 
 # Montar Google Drive
-#drive.mount('/content/drive')
+drive.mount('/content/drive')
 
 # Ruta del archivo en Google Drive
-#file_path = '/content/drive/MyDrive/COLABORATORY/Pycaret/dataset03'
+file_path = '/content/drive/MyDrive/COLABORATORY/Pycaret/dataset03'
 
 # Leer el archivo CSV
-#df = pd.read_csv(file_path, header=None, names=['FONDO', 'CONCURSO', 'CONVOCATORIA', 'ANIO', 'MONTO_FINANCIAMIENTO', 'MONEDA'])
+df = pd.read_csv(file_path, header=None, names=['FONDO', 'CONCURSO', 'CONVOCATORIA', 'ANIO', 'MONTO_FINANCIAMIENTO', 'MONEDA'])
 #-----------------
-df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
-                names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+#df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
+                #names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
 
 st.title('Análisis exploratorio monto financiero de convocatoria')
 st.header('Esta aplicación permite explorar el conjunto de datos de monto financiero de convocatoria mediante diversos gráficos.')
